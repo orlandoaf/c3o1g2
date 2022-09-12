c54c13b1d4426e29867fc9a5bae8b7303b9fccb8
eburgos97 <111750336+eburgos97@users.noreply.github.com>
2022-09-12 18:34:11 -0500

    NetBeans

---
+++ tiendamibarrio/src/java/logica/DemoClientes.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author Eburg
+ */
+public class DemoClientes {
+    public static void main(String[] args) throws SQLException{
+        
+        Cliente a = new Cliente();
+        //Agregar nuevo cliente
+        a.setIdCliente("CLI-002");
+//        a.setNombre("Ricardo Pablo");
+//        a.setDireccion("Cali");
+//        a.setTelefono("3102323232");
+//        a.setEmail("a@username.com");
+//        System.out.println(a.guardarCliente());
+//        System.out.println(a.actualizarCliente());
+        System.out.println(a.eliminarCliente());
+
+        List<Cliente> clientes = a.consultarclientes();
+        for(Cliente ax: clientes){
+            System.out.println(ax.toString());
+        }
+    }
+    
+}
